57611c9cfd182be7f0ad05b30e93e86e5084d532
Dheeraj6778 <110718183+Dheeraj6778@users.noreply.github.com>
2022-08-18 17:39:05 +0530

    Delete 2022201022_Q4.sql

--- 2022201022_Q4.sql
+++
@@ -1,3 +0,0 @@
-select count(Dnumber) from dept_locations where Dnumber in
-(select Dnumber from department where Mgr_ssn in 
-(select Essn from dependent group by Essn having count(Sex='F')>=2)) group by Dnumber;
