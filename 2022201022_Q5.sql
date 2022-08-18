5411314124ca7b91a5f74d5a53f5e990adb2d27e
Dheeraj6778 <110718183+Dheeraj6778@users.noreply.github.com>
2022-08-18 17:39:14 +0530

    Delete 2022201022_Q5.sql

--- 2022201022_Q5.sql
+++
@@ -1,3 +0,0 @@
-select count(Essn) from dependent where Essn in
-(select Mgr_ssn from department where Dnumber in 
-(select Dnumber from dept_locations group by Dnumber having count(Dnumber)>1)) group by Essn;
